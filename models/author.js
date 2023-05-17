8fa8664929be122db8e021ab283d7e3cfe9bd508
Henry Ripple <hripple46@gmail.com>
2023-05-17 13:31:39 -0500

    formatted author dates

--- models/author.js
+++ models/author.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   family_name: { type: String, required: true, maxLength: 100 },
-  date_of_birth: { type: Date },
-  date_of_death: { type: Date },
+  date_of_birth: { type: Date, default: null },
+  date_of_death: { type: Date, default: null },
 });
 
 AuthorSchema.virtual("name").get(function () {
@@ -21,6 +22,24 @@ AuthorSchema.virtual("name").get(function () {
 
 AuthorSchema.virtual("url").get(function () {
   return "/catalog/author/" + this._id;
+});
+
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  console.log(this.date_of_birth);
+  if (!this.date_of_birth) {
+    return "Unknown";
+  }
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+    DateTime.DATE_MED
+  );
+});
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  if (!this.date_of_death) {
+    return "Present";
+  }
+  return DateTime.fromJSDate(this.date_of_death).toLocaleString(
+    DateTime.DATE_MED
+  );
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
